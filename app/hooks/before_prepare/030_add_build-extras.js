c8e113d182ee2d3c3f57db3a13236ef7d527cf63
Daniel Campagnoli <campers@gmail.com>
2016-05-31 22:56:15 +0800

    Update cordova hook scripts to fix dependancy issues The Facebook plugin is using an older version of bolts than the Parse plugin, so we exclude the Facebook one (com.parse.bolts:bolts-android)

--- app/hooks/before_prepare/030_add_build-extras.js
+++ app/hooks/before_prepare/030_add_build-extras.js
@@ -1,18 +1,22 @@
 #!/usr/bin/env node
 
-// Custom hook to fix the conflict between the AdMob plugin and the Facebook plugin
+// Custom hook to fix the conflict between the AdMob plugin and the Facebook plugin and Parse plugin
 // http://stackoverflow.com/questions/32099385/error-multiple-dex-files-define-landroid-support-annotations-animres-with-admob
+// https://github.com/BoltsFramework/Bolts-Android/issues/80
 
-var fs = require('fs');
-var path = require('path');
+var fs = require('fs')
+var path = require('path')
 
-var rootDir = process.argv[2];
+var rootDir = process.argv[2]
 
 var androidPlatformDir = rootDir + '/platforms/android/'
 var buildExtrasGradle = androidPlatformDir + 'build-extras.gradle'
 
 if(fs.existsSync(androidPlatformDir) && !fs.existsSync(buildExtrasGradle)) {
-	var fileContents = "configurations { all*.exclude group: 'com.android.support', module: 'support-v4' }"
+	console.log('Creating ' + buildExtrasGradle)
+	var fileContents = "configurations { all*.exclude group: 'com.android.support', module: 'support-v4' }\n"
+					+ "configurations { all*.exclude group: 'com.parse.bolts', module: 'bolts-android' }"
+
 	fs.writeFileSync(buildExtrasGradle, fileContents, 'utf8')
 	console.log('Created ' + buildExtrasGradle)
 }
