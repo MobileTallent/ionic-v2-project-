b58ef1fa7767df59dfefbc6c99fece565831ff9c
Daniel Campagnoli <campers@gmail.com>
2016-06-11 13:46:04 +0800

    Update the build-extra.gradle generation Remove the ionic platform restore. Will get to add them manually

--- app/hooks/before_prepare/030_add_build-extras.js
+++ app/hooks/before_prepare/030_add_build-extras.js
@@ -3,6 +3,9 @@
 // Custom hook to fix the conflict between the AdMob plugin and the Facebook plugin and Parse plugin
 // http://stackoverflow.com/questions/32099385/error-multiple-dex-files-define-landroid-support-annotations-animres-with-admob
 // https://github.com/BoltsFramework/Bolts-Android/issues/80
+// http://stackoverflow.com/questions/28791817/android-studio-gradle-error-multiple-dex-files-define
+
+// http://stackoverflow.com/questions/31727318/cordova-multiple-dex-files-define-annotation
 
 var fs = require('fs')
 var path = require('path')
@@ -11,12 +14,22 @@ var rootDir = process.argv[2]
 
 var androidPlatformDir = rootDir + '/platforms/android/'
 var buildExtrasGradle = androidPlatformDir + 'build-extras.gradle'
-/*
-if(fs.existsSync(androidPlatformDir) && !fs.existsSync(buildExtrasGradle)) {
-	console.log('Creating ' + buildExtrasGradle)
-	var fileContents = "configurations { all*.exclude group: 'com.android.support', module: 'support-v4' }\n"
-					+ "configurations { all*.exclude group: 'com.parse.bolts', module: 'bolts-android' }"
+
+if(fs.existsSync(androidPlatformDir)) {
+
+	var fileContents = "// This file is written from /app/hooks/before_prepare/030_add_build-extras.js\n"
+		+ "configurations { all*.exclude group: 'com.android.support', module: 'support-v4' }\n"
+		+ "configurations { all*.exclude group: 'com.parse.bolts', module: 'bolts-android' }\n"
+		+ "configurations { all*.exclude group: 'com.parse.bolts', module: 'bolts-tasks' }\n"
+		//+ "dependencies { compile 'com.parse.bolts:bolts-applinks:1.4.0' }\n"
+
+		+ "dependencies { compile ('com.facebook.android:facebook-android-sdk:4.8.+'){ exclude group: 'com.parse.bolts', module: 'bolts-tasks' exclude group: 'com.parse.bolts', module: 'bolts-applinks';} }\n"
+
+	//	+ "dependencies {compile 'com.parse.bolts:bolts-tasks:1.4.0'}\n"
+		+ "dependencies {compile 'com.parse.bolts:bolts-applinks:1.4.0'}\n"
+	
+	console.log('Updating ' + buildExtrasGradle)
+	console.log(fileContents)
 
 	fs.writeFileSync(buildExtrasGradle, fileContents, 'utf8')
-	console.log('Created ' + buildExtrasGradle)
-}*/
+}
