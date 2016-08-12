919fe2258aea038798b40b52af71c29b1ef24b5f
Daniel Campagnoli <campers@gmail.com>
2016-08-12 09:00:29 -0700

    Created a profileImageUrl and fileUrl filter to start reducing the dependancy on the functions added to the Parse objects. Also fixes some issues with displaying images. Created profileAge filter which works for both current and other use profiles

---
+++ app/app/js/util/file-url.filter.ts
@@ -0,0 +1,18 @@
+/**
+ * Filter which returns the URL for a File object
+ */
+angular.module('ionicApp').filter('fileUrl', function() {
+	return function(file:any):string {
+
+		if(!file)
+			return null
+		if(file._url)
+			return file._url
+		if(angular.isFunction(file.url))
+			return file.url()
+		if(angular.isString(file.url))
+			return file['url']
+
+		throw 'unknown file ' + JSON.stringify(file)
+	}
+})
