e1825d335fe1711703cc3d8618f99f4ae7b94850
Daniel Campagnoli <daniel@apporchestra.com>
2016-10-01 02:22:07 +0800

    Create node script for deploying to Google App Engine so npm run deploy-prod/qa script works on Windows

---
+++ server/deploy-gae.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+// Script to deploy to Google App Engine
+var shell = require('shelljs')
+var fs = require('fs')
+
+var config = require('./config.json');
+
+// Get the --env arg
+var env = require('commander')
+    .option('-e, --env <environment>', 'The environment to deploy to')
+    .parse(process.argv).env
+if(!env) env = 'dev'
+
+// The the env config exists in config.json
+if(!config[env]) {
+    console.log('Configuration for the ' + env + ' environment does not exist in config.json')
+    process.exit(1)
+}
+
+// Copy the .yaml file
+var envYaml = 'app-' + env + '.yaml'
+
+if(!fs.existsSync(envYaml)) {
+    console.log('The file ' + envYaml + ' does not exist to deploy to the environment ' + env)
+    process.exit(1)
+}
+
+shell.cp(envYaml, 'app.yaml')
+
+// Deploy!
+var child = shell.exec('gcloud app deploy --quiet --stop-previous-version --project cloneder-prod ');
