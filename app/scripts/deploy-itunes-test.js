a2e43e1da91ee7d5f5ca6b2b8ed361e30d96f6f6
Daniel Campagnoli <daniel@apporchestra.com>
2016-10-24 17:38:03 +0800

    Update npm scripts to run on windows and have error checking

---
+++ app/scripts/deploy-itunes-test.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+var shell = require('shelljs')
+var xml2js = require('xml2js')
+var fs = require('fs')
+
+var parser = new xml2js.Parser();
+var config = parser
+
+fs.readFile(__dirname + '/config.xml', function(err, data) {
+
+    if(err) {
+        console.log('Error reading config.xml')
+        console.log(err)
+        process.exit(1)
+    }
+
+    parser.parseString(data, function (err, result) {
+
+        if(err) {
+            console.log('Error parsing config.xml')
+            console.log(err)
+            process.exit(1)
+        }
+
+        if(!result.widget.name) {
+            console.log('Configure the name in config.xml')
+            process.exit(1)
+        }
+
+        var filename = `platforms/ios/build/device/${result.widget.name}.ipa`
+        if(!fs.existsSync(filename)) {
+            console.log(filename + ' doesnt exist. Have you done a build for a device?')
+            console.log('Note that the ionic build --device --release command can take a few runs to actually build the .ipa file')
+            console.log('You should see the last line of the console output')
+            process.exit(1)
+        }
+
+        var command = `pilot upload --ipa \"platforms/ios/build/device/${result.widget.name}.ipa\" --distribute_external --skip_waiting_for_build_processing`
+        console.log('exec: ' + command)
+        shell.exec(command)
+    })
+})
---
+++ server/scripts/dashboard.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+// Script to run the Parse dashboard
+var shell = require('shelljs')
+var fs = require('fs')
+
+var config = require('./config.json');
+
+// Get the --env arg
+var env = require('commander')
+    .option('-e, --env <environment>', 'The environment to deploy to')
+    .parse(process.argv).env
+if(!env) env = 'dev'
+
+// The the env config exists in config.json
+if(!config[env]) {
+    console.log('Configuration for the ' + env + ' environment does not exist in config.json')
+    process.exit(1)
+}
+
+if(!config.parseMount) {
+    console.log('parseMount is not configured in config.json')
+    process.exit(1)
+}
+
+if(!config.parseAppId) {
+    console.log('parseAppId is not configured in config.json')
+    process.exit(1)
+}
+
+if(!config.parseMasterKey) {
+    console.log('parseAppId is not configured in config.json')
+    process.exit(1)
+}
+
+if(!config[env].serverUrl) {
+    console.log('serverUrl is not configured in config.json for the environment ' + env)
+    process.exit(1)
+}
+
+var command = `parse-dashboard --serverURL \"${config.prod.serverUrl}${config.parseMount}\" --appId ${config.json.parseAppId} --masterKey ${config.parseMasterKey}`
+console.log('exec: ' + command)
+var child = shell.exec(command);
