ef5242841be4381a0dce7fd337ebea441fbcbdbc
GEIDI\JosephR <josephr@geidi.com>
2017-06-01 15:13:38 +0800

    clean up

--- app/app/js/admin/graph.ts
+++ app/app/js/admin/graph.ts
@@ -8,11 +8,6 @@ module app {
         public matchData = []
         public chatsData = [28, 48, 40, 19, 86, 27, 90]
         public data = [[]]
-            
-            
-        // public onClick = function (points, evt) {
-        //     console.log(points, evt);
-        // };
         public datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
         public options = {
             scales: {
@@ -32,7 +27,7 @@ module app {
                 ]
             }
         }
-        
+
         constructor(private $log: ng.ILogService, private $scope: ng.IScope,
             private AppService: IAppService, private AppUtil: AppUtil) {
             $scope.$on('$ionicView.beforeEnter', () => this.getReport())
@@ -47,45 +42,45 @@ module app {
             this.voterReport = null
             this.data = [[]]
 
-            while(dateCovered <= curDate){
+            while (dateCovered <= curDate) {
                 this.labels.push(dateCovered.toDateString())
-                dateCovered.setDate(dateCovered.getDate() + 1);
+                dateCovered.setDate(dateCovered.getDate() + 1)
             }
             this.AppUtil.blockingCall(
                 this.AppService.getMatchesReport(this.numDays),
                 items => {
                     this.voterReport = {}
                     this.$log.log('LOADED ' + items.length + ' items')
-                    
+
                     items.forEach(a => {
                         if (a.matchedDate.toDateString() in this.voterReport)
                             this.voterReport[a.matchedDate.toDateString()]++
                         else
-                            this.voterReport[a.matchedDate.toDateString()] = 1;
+                            this.voterReport[a.matchedDate.toDateString()] = 1
                     })
                     this.labels.forEach(a => {
                         if (a in this.voterReport)
-                            this.matchData.push(this.voterReport[a])    
+                            this.matchData.push(this.voterReport[a])
                         else
-                            this.matchData.push(0)    
+                            this.matchData.push(0)
                     })
                     this.data[0] = this.matchData
 
-                    this.AppService.getChatMessageReport(this.numDays).then(                
+                    this.AppService.getChatMessageReport(this.numDays).then(
                         items => {
                             this.$log.log('loaded ' + items.length + ' chats')
-                            var groupByDateObj = _.groupBy(items, function(n) {
+                            var groupByDateObj = _.groupBy(items, function (n) {
                                 return n.createdAt.toDateString()
                             })
 
                             for (var item in groupByDateObj) {
-                                var arrayOfNames = _.groupBy(item, function(n) {
+                                var arrayOfNames = _.groupBy(item, function (n) {
                                     return n.senderName
                                 })
-                                console.log("Names length: " + arrayOfNames.length)
+                                console.log('Names length: ' + arrayOfNames.length)
                             }
-                    })
-            })
+                        })
+                })
         }
     }
 
