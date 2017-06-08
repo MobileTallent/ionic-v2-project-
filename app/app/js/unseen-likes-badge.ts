e258c074a97a35aa7ca5e3fc14359ef8e26501f9
Byron <b.lewis.design.development@gmail.com>
2017-06-08 16:53:27 +1000

    JUS-384 - New Likes Badge
    Unseen likes badge

---
+++ app/app/js/unseen-likes-badge.ts
@@ -0,0 +1,19 @@
+
+/**
+ * A directive to display an unread count badge
+ */
+
+angular.module('ionicApp').directive('unseenLikesBadge', function (AppService: app.IAppService) {
+	return {
+		restrict: 'E',
+		scope: {},
+		template: '<span ng-show="likesCount > 0" class="badge badge-assertive nav-icon-badge">{{likesCount}}</span>',
+		replace: true, // Need replace otherwise when its used in the menu bar it doesnt work
+		controller: function ($scope) {
+			AppService.getProfilesWhoLikeMe()
+				.then(profiles => {
+					$scope.likesCount = profiles.length
+				})
+		}
+	}
+});
