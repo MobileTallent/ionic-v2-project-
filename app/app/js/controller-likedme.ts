35f0fd28e5822671c028cd0f77669bcf84a2bb88
GEIDI\JosephR <josephr@geidi.com>
2017-06-16 15:44:47 +0800

    JUS 762 - Share functionality is not working on People who liked me page.

--- app/app/js/controller-likedme.ts
+++ app/app/js/controller-likedme.ts
@@ -12,16 +12,19 @@ module app {
 		private $ionicModal
 		private $ionicPopup
 		private $ionicSlideBoxDelegate
+		private $cordovaSocialSharing
 		private AppService: IAppService
 		private AppUtil: AppUtil
 		public profiles: IProfile[]
+		private linkToBeShared
+		public isLoading
 
 		private profileModal
 		public profile // The profile being viewed
 		public profileIndex // The index in profiles of the profile viewed
 
 		constructor($log: ng.ILogService, $rootScope: app.IAppRootScope, $scope: ng.IScope,
-			$state, $ionicModal, $ionicPopup, AppService: IAppService, AppUtil, $ionicSlideBoxDelegate) {
+			$state, $ionicModal, $ionicPopup, AppService: IAppService, AppUtil, $ionicSlideBoxDelegate, $cordovaSocialSharing) {
 			$log.info('LikedMe constructor')
 			this.$log = $log
 			this.$rootScope = $rootScope
@@ -32,18 +35,18 @@ module app {
 			this.AppService = AppService
 			this.AppUtil = AppUtil
 			this.$ionicSlideBoxDelegate = $ionicSlideBoxDelegate
+			this.$cordovaSocialSharing = $cordovaSocialSharing
 
-			$ionicModal.fromTemplateUrl('profileModal.html', {
+			this.$ionicModal.fromTemplateUrl('profileModal.html', {
 				scope: $scope,
 				animation: 'slide-in-up'
 			}).then(modal => this.profileModal = modal)
 			// Cleanup the modal when we're done with it
-			$scope.$on('$destroy', () => this.profileModal.remove())
-
+			this.$scope.$on('$destroy', () => this.profileModal.remove())
 			this.refresh()
 		}
 
-		public refresh() {
+		private refresh() {
 			this.AppService.getProfilesWhoLikeMe()
 				.then(profiles => {
 					this.profiles = profiles
@@ -72,6 +75,61 @@ module app {
 			this.process(false)
 		}
 
+		public share() {
+			this.isLoading = true
+			if (typeof Branch !== 'undefined') {
+				// only canonicalIdentifier is required
+				var contentDescriptionText = 'Just a Baby is a brand new app connecting people who want to make a baby.'
+				contentDescriptionText = contentDescriptionText + ' We can help you find a surrogate, partner, co-parent,'
+				contentDescriptionText = contentDescriptionText + ' sperm or egg donor - or find someone that needs your help to have a baby.'
+				var properties = {
+					canonicalIdentifier: this.profile.id ? this.profile.id : this.profile.objectId,
+					canonicalUrl: 'https://justababy.com/',
+					title: 'A brand new way to make babies. Start your journey today.',
+					contentDescription: contentDescriptionText,
+					contentImageUrl: this.getPhotoUrl(this.profile.photos)
+				}
+
+				// create a branchUniversalObj variable to reference with other Branch methods
+				Branch.createBranchUniversalObject(properties).then(res => {
+					var analyticsLink = {
+						channel: 'facebook',
+						feature: 'sharing',
+						tags: ['JustaBaby', 'justababy']
+					}
+
+					// optional fields
+					var properties1 = {
+						$desktop_url: 'https://justababy.com/',
+						$android_url: 'https://play.google.com/store/apps/details?id=co.justababy.app',
+						$ios_url: 'https://itunes.apple.com/us/app/just-a-baby/id1147759844?mt=8',
+						profileId: this.profile.id ? this.profile.id : this.profile.objectId
+					}
+					if (res) {
+						res.generateShortUrl(analyticsLink, properties1).then(link => {
+							this.linkToBeShared = JSON.stringify(link.url)
+							var profileShare = "This person wants to have or help others make a baby: "
+							profileShare = profileShare + "\n\n\"" + this.profile.about.toString() + "\"\n\nThought they could be a good match for you? \n\n"
+							this.isLoading = false
+							this.$cordovaSocialSharing.share(profileShare, null, null, this.linkToBeShared) // Share via native share sheet
+								.then(() => {
+									if (typeof analytics !== 'undefined') {
+										analytics.trackView('Share This Profile')
+									}
+									this.$log.debug('Social share action complete')
+								}, error => {
+									this.$log.error('Social share action error ' + JSON.stringify(error))
+								})
+						}).catch(function (err) {
+							alert('Error in Branch URL: ' + JSON.stringify(err))
+						})
+					}
+				}).catch(function (err) {
+					alert('Error in creating Uni Obj: ' + JSON.stringify(err))
+				})
+			}
+		}
+
 		private process(liked: boolean) {
 			this.AppUtil.blockingCall(
 				this.AppService.processMatch(this.profile, liked),
@@ -79,6 +137,20 @@ module app {
 					this.profiles.splice(this.profileIndex, 1)
 					this.close()
 				})
+		}
+
+		private getPhotoUrl(photos) {
+			if (photos && photos.length) {
+				var photo = photos[0]
+				if (photo._url)
+					return photo._url
+				if (angular.isFunction(photo.url))
+					return photo.url()
+				if (angular.isString(photo.url))
+					return photo.url
+			}
+
+			return 'img/generic_avatar.jpg'
 		}
 
 		public close() {
@@ -103,7 +175,7 @@ module app {
 
 
 	LikedMe.$inject = ['$log', '$rootScope', '$scope', '$state', '$ionicModal',
-	 '$ionicPopup', 'AppService', 'AppUtil', '$ionicSlideBoxDelegate']
+		'$ionicPopup', 'AppService', 'AppUtil', '$ionicSlideBoxDelegate', '$cordovaSocialSharing']
 
 	angular.module('controllers').controller('LikedMe', LikedMe)
 }
