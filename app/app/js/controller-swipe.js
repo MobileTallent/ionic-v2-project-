c782fc104b85abd6aab2e01497d50d58a0adae6d
GEIDI\JosephR <josephr@geidi.com>
2017-07-05 14:34:07 +0800

    recommit

--- app/app/js/controller-swipe.js
+++ app/app/js/controller-swipe.js
@@ -18,19 +18,23 @@ angular.module('controllers')
 
     //$scope.cloned_profile = profile.clone()
 
-/**----------------------------------------------
-    SEARCH FILTERS
-  -----------------------------------------------  
-*/
+    /**----------------------------------------------
+        SEARCH FILTERS
+      -----------------------------------------------  
+    */
     $scope.isFiltersOpened = false
     $scope.iprofile = profile.clone();
     $scope.showMI = $scope.profile.distanceType === 'mi' ? true : false
     $scope.showKM = $scope.profile.distanceType === 'km' ? true : false
     var dType = $scope.profile.distanceType
-     var fields = ['enabled', 'guys', 'girls', 'ageFrom', 'ageTo', 'notifyMatch', 'notifyMessage', 'distanceType', 'distance', 'LFSperm', 'LFEggs', 'LFWomb', 'LFEmbryo', 'LFNot', 'LFHelpM', 'LFHelpO', 'LFSelfId']
+    var fields = ['enabled', 'guys', 'girls', 'ageFrom', 'ageTo', 'notifyMatch', 'notifyMessage', 'distanceType', 'distance', 'LFSperm', 'LFEggs', 'LFWomb', 'LFEmbryo', 'LFNot', 'LFHelpM', 'LFHelpO', 'LFSelfId']
 
     if (!$scope.profile.LFSelfId) {
         $scope.profile.LFSperm = $scope.profile.LFEggs = $scope.profile.LFWomb = $scope.profile.LFEmbryo = true
+    }
+
+    if (!$scope.profile.LFIndividual && !$scope.profile.LFCouple) {
+        $scope.profile.LFIndividual = $scope.profile.LFCouple = true
     }
 
     $scope.setLanguage = (key) => {
@@ -60,10 +64,10 @@ angular.module('controllers')
         AppUtil.blockingCall(
             AppService.saveProfile(_.pick($scope.iprofile, fields)),
             () => {
-                
+
                 $ionicLoading.hide()
-                $state.go($state.current, {}, {reload: true});
-                
+                $state.go($state.current, {}, { reload: true });
+
             }, 'SETTINGS_SAVE_ERROR'
         )
     }
@@ -73,13 +77,13 @@ angular.module('controllers')
             if ($scope.searchFiltersModal && $scope.isFiltersOpened)
                 $scope.save()
         }, 200);
-        
+
     })
-    
+
     //  //  //  //  //  SEARCH FILTERS  \\  \\  \\  \\  \\
 
 
-        $scope.changeIndividual = () => {
+    $scope.changeIndividual = () => {
         if ($scope.profile.LFIndividual === false && $scope.profile.LFCouple === false) {
             $scope.profile.LFCouple = true;
         }
@@ -172,24 +176,28 @@ angular.module('controllers')
     }
 
     $scope.openSearchFilters = function() {
+        if (typeof analytics !== 'undefined') {
+            analytics.trackView("Search Filters Controller")
+        }
         $scope.searchFiltersModal.show()
-            .then(() => $scope.isFiltersOpened = true )
+            .then(() => $scope.isFiltersOpened = true)
     }
+
 
     $scope.hideSearchFilters = function() {
         $scope.searchFiltersModal.hide()
-            .then(() => $scope.isFiltersOpened = false )
+            .then(() => $scope.isFiltersOpened = false)
     }
 
     $ionicModal.fromTemplateUrl('searchFiltersModal.html', {
-            scope: $scope,
-            animation: 'slide-in-up'
-        }).then(searchFiltersModal => $scope.searchFiltersModal = searchFiltersModal)
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(searchFiltersModal => $scope.searchFiltersModal = searchFiltersModal)
 
-/**----------------------------------------------
-    END OF SEARCH FILTERS
-  -----------------------------------------------  
-*/
+    /**----------------------------------------------
+        END OF SEARCH FILTERS
+      -----------------------------------------------  
+    */
     $scope.deleteUnmatchedSwipes = () => AppUtil.blockingCall(
         AppService.deleteUnmatched(),
         success => $log.log(success),
@@ -241,21 +249,21 @@ angular.module('controllers')
 
     function checkQuotaTime() {
         var quotaNotReached = false
-        
+
         if (profile.quotaSearchedDate) {
-            
+
             var twelveHrsAgo = 43200000 // 43200000-12hrs  Formula: hrs*mins*sec*1000ms  12*60*60*1000
             var now = new Date()
             var now_utc = new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds())
             var diff = now_utc - profile.quotaSearchedDate
             var timeLimitDatetime = twelveHrsAgo - diff
             var timeRemaining = parseTimeRemaining(timeLimitDatetime)
-            
+
             if (diff < twelveHrsAgo) {
 
                 $scope.timeRemaining = timeRemaining
                 $scope.timeLimitDatetime = new Date(timeLimitDatetime)
-                
+
                 $scope.$broadcast('likeLimitReached')
                 quotaNotReached = true
             }
@@ -339,15 +347,14 @@ angular.module('controllers')
         $scope.openModal(2)
 
         $scope.timeLimitCounter = $interval(() => {
-            
+
             if ($scope.timeLimitDatetime) {
                 $scope.timeLimitDatetime.setSeconds($scope.timeLimitDatetime.getSeconds() - 1)
                 $scope.timeRemaining = parseTimeRemaining($scope.timeLimitDatetime.getTime())
+            } else {
+                $state.go($state.current, {}, { reload: true });
             }
-            else {
-                $state.go($state.current, {}, {reload: true});
-            }
-            
+
         }, 1000)
 
     })
